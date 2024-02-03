dab8ee467e82b968cc1719942cdad09790598a0c
Sheldon <36916808+slmwest@users.noreply.github.com>
2024-02-03 18:05:18 +0000

    finish graph API implementation

--- Assets/Graphs/Graph.cs
+++ Assets/Graphs/Graph.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Graph : MonoBehaviour
+public class Graph
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Edge> edges = new List<Edge>();
+    public List<Node> nodes = new List<Node>();
+    public List<Node> pathList = new List<Node>();
+
+    public Graph() { }
+
+    public void AddNode(GameObject id)
     {
-        
+        Node node = new Node(id);
+        nodes.Add(node);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddEdge(GameObject fromNode, GameObject toNode)
     {
-        
+        Node from = FindNode(fromNode);
+        Node to = FindNode(toNode);
+
+        if (from != null && to != null)
+        {
+            Edge e = new Edge(from, to);
+            edges.Add(e);
+            from.edgeList.Add(e);
+        }
+    }
+    Node FindNode(GameObject id)
+    {
+        foreach (Node n in nodes)
+        {
+            if (n.getId() == id)
+                return n;
+        }
+        return null;
     }
 }
+
