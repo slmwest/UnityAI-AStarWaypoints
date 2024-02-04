5e60f25d4df86e2f820e58cfe3481e23be7b8161
Sheldon <36916808+slmwest@users.noreply.github.com>
2024-02-04 08:13:52 +0000

    improve tracker with finer accuracy on end goal relative to intermittant points

--- Assets/FollowWaypoints.cs
+++ Assets/FollowWaypoints.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class FollowWaypoints : MonoBehaviour
 {
+    [Header("TamkMovement")]
     Transform goal;
     public float speed = 5.0f;
-    public float accuracy = 3.0f;
+    public float accuracy = 0.5f;
     public float rotSpeed = 2.5f;
-    public int lookAhead = 5;
-    GameObject tracker;
 
+    [Header("TrackerMovement")]
+    GameObject tracker;
+    public float trackerBoost = 1.1f;
+    public int lookAhead = 5;
+    float accuracyBoost;
+
+    [Header("Waypoint System")]
     public GameObject wpManager;
     GameObject[] wps;
     GameObject currentNode;
@@ -60,6 +66,14 @@ public class FollowWaypoints : MonoBehaviour
         currentWP = 0;
     }
 
+    public void GoToRockTower()
+    {
+        Debug.Log("Going to rock tower!");
+        int targetNodeIdx = 15;
+        g.AStar(currentNode, wps[targetNodeIdx]);
+        currentWP = 0;
+    }
+
     void ProgressTracker()
     {
         // at goal
@@ -69,31 +83,37 @@ public class FollowWaypoints : MonoBehaviour
         if (Vector3.Distance(tracker.transform.position, this.transform.position) >= lookAhead) { return; }
 
         // move tracker forward to next waypoint with no interpolation on rotation
-        tracker.transform.LookAt(g.pathList[currentWP].getId().transform.position);
-        tracker.transform.Translate(0, 0, (speed + 0.4f) * Time.deltaTime);
+        goal = g.pathList[currentWP].getId().transform;
+        tracker.transform.LookAt(new Vector3(goal.position.x,
+                                            this.transform.position.y, // in case of any inconsistency with y-coords of the invisible WPs!
+                                            goal.position.z));
+        tracker.transform.Translate(0, 0, (speed * trackerBoost) * Time.deltaTime);
 
-        // progress to next way point
-        if (Vector3.Distance(tracker.transform.position, g.pathList[currentWP].getId().transform.position) < accuracy)
+        // progress to next way point if additional waypoints remain
+        accuracyBoost = currentWP < g.pathList.Count -1 ? 4 : 1;
+        if (Vector3.Distance(tracker.transform.position, g.pathList[currentWP].getId().transform.position) < accuracyBoost * accuracy)
         {
             currentNode = g.pathList[currentWP].getId();
             currentWP++;
-            Debug.Log(currentWP);
+            //Debug.Log(currentWP + ", " + (accuracyBoost * accuracy));
         }
 
     }
 
+    // my attempt at the challenge!
     void LateUpdate()
     {
-        // at goal
+        // at goal or no goal to follow
+        if (g.pathList.Count == 0) { return; }
         if (currentWP >= g.pathList.Count && Vector3.Distance(tracker.transform.position, this.transform.position) < accuracy) { return; }
 
         ProgressTracker();
 
-        //this.transform.LookAt(waypoints[currentWP].transform.position); // snap to look at new target
+        // move tank to follow the tracker
         Quaternion lookAtWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAtWP, rotSpeed * Time.deltaTime);
-
         this.transform.Translate(0, 0, speed * Time.deltaTime);
 
     }
+
 }
